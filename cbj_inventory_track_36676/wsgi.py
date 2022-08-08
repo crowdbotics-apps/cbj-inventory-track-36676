ed8925c7090d7226d91da36e4d17586fd534f251
Crowdbotics <team@crowdbotics.com>
2022-08-08 22:59:07 +0000

    Initial commit.

---
+++ cbj_inventory_track_36676/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cbj_inventory_track_36676 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cbj_inventory_track_36676.settings')
+
+application = get_wsgi_application()
